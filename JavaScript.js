d541531541af5cfd8c33ca01c6f565df63e6d046
Adrian Pelaz Medrano <71392163+AdrianPelaz@users.noreply.github.com>
2021-01-17 23:01:10 +0100

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,22 @@
+
+function opacidad1(opacidad1) {
+    document.getElementById("marca1").style.opacity=opacidad1;
+}
+function opacidad2(opacidad2) {
+    document.getElementById("marca2").style.opacity=opacidad2;
+}
+function opacidad3(opacidad3) {
+    document.getElementById("marca3").style.opacity=opacidad3;
+}
+function opacidad4(opacidad4) {
+    document.getElementById("marca4").style.opacity=opacidad4;
+}
+function opacidad5(opacidad5) {
+    document.getElementById("PcComponentes").style.opacity=opacidad5;
+}
+function opacidad6(opacidad6) {
+    document.getElementById("Amazon").style.opacity=opacidad6;
+}
+function opacidad7(opacidad7) {
+    document.getElementById("Mediamarkt").style.opacity=opacidad7;
+}
